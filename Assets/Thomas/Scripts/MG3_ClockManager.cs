918926a257a99314ce0f3493ec87424caac03020
Thoma-fr <78789215+Thoma-fr@users.noreply.github.com>
2023-09-08 11:42:43 +0200

    maj minigame

--- Assets/Thomas/Scripts/MG3_ClockManager.cs
+++ Assets/Thomas/Scripts/MG3_ClockManager.cs
@@ -12,7 +12,7 @@ public class MG3_ClockManager : MonoBehaviour
     [SerializeField] private Vector3 startCamPos;
     [SerializeField] private float camspeed;
     private bool canInput;
-
+    private bool canScore=true;
     public AudioClip ring;
     void Start()
     {
@@ -63,26 +63,29 @@ public class MG3_ClockManager : MonoBehaviour
     }
     public void Checkcolor(int ID, GameColor col)
     {
-        if (!canInput)
+        if (canScore)
         {
-            if (ID == 1)
-                EndMinigame(2);
-            else
-                EndMinigame(1);
-            StopAllCoroutines();
-        }
-        else
-        {
-            if(col== colorAvailable)
-            {
-                EndMinigame(ID);
-            }
-            else
+            if (!canInput)
             {
                 if (ID == 1)
                     EndMinigame(2);
                 else
                     EndMinigame(1);
+                StopAllCoroutines();
+            }
+            else
+            {
+                if (col == colorAvailable)
+                {
+                    EndMinigame(ID);
+                }
+                else
+                {
+                    if (ID == 1)
+                        EndMinigame(2);
+                    else
+                        EndMinigame(1);
+                }
             }
         }
     }
@@ -124,6 +127,7 @@ public class MG3_ClockManager : MonoBehaviour
     {
         GetComponent<AudioSource>().Stop();
         GetComponent<AudioSource>().PlayOneShot(ring);
+        canScore= false;
         Sequence sequence = DOTween.Sequence();
         sequence.Append(transform.DOLocalRotate(new Vector3(0, 0, 5),0.1f,RotateMode.Fast));
         sequence.Append(transform.DOLocalRotate(new Vector3(0, 0, -5), 0.1f, RotateMode.Fast).SetLoops(-1,LoopType.Restart));
